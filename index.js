ee3be0147a86fbf9bd038eb3b9eaae23c10ca393
Tess <tess.merrill25@gmail.com>
2023-07-03 13:21:04 -0600

    complete array lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+};
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+};
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+};
+
+function appendCat() {
+    const lastCats = [...cats, "Broom"]
+    return lastCats;
+};
+function prependCat() {
+    const firstCats = ["Arnold", ...cats]
+    return firstCats;
+};
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+};
+
+function removeFirstCat() {
+    return cats.slice(1);
+};
+
+
+// append cat
+// prepend cat
+// remove last cat
+// remove first cat
